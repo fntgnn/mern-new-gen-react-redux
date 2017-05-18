091db762894513ab2a229de255f76f609afa1650
Giovanni <gio.fon@gmail.com>
2017-05-18 15:36:15 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  }
+};
